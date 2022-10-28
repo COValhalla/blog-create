c625d018a60ec7f903b16fda7a139cc44e43d0e7
COValhalla <josephvellella@proton.me>
2022-10-28 11:38:11 -0600

    Added edit blog link.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Create from './components/Create'
+import Edit from './components/Edit'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
 import Navigation from './components/partials/Navigation'
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
+          <Route path="/edit/" element={<Edit />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
         </Routes>
