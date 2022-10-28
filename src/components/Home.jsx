d28501a471bca40aa65a89b3c073d4544048b42d
COValhalla <josephvellella@proton.me>
2022-10-28 11:37:49 -0600

    Retrieve blogs, if valid token. Edit blog link.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,58 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
 
 function Home() {
+  const context = useContext(UserContext)
+  const [blogs, setBlogs] = useState([])
+  const [errorMessages, setErrorMessages] = useState([])
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const res = await fetch('http://localhost:3000/api/blogs', {
+          headers: {
+            Authorization: `Bearer ${context.token}`,
+          },
+        })
+
+        const data = await res.json()
+
+        if (res.status === 200) {
+          setBlogs(data.blogs)
+        } else {
+          setErrorMessages((prevErrorMessages) => {
+            const newErrorMessages = prevErrorMessages.filter(
+              (message) => message !== data.errors,
+            )
+            newErrorMessages.push(data.errors)
+            return newErrorMessages
+          })
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchBlogs()
+  }, [context.token])
+
   return (
-    <div>
-      <h1 className="">Implement Sign-In/Auth for Creating Blogs Later</h1>
+    <div className="flex flex-col items-center gap-2">
+      <h1 className="text-2xl font-bold">Blogs</h1>
+      <div className="flex w-1/2 flex-col gap-2">
+        {blogs.map((blog) => (
+          <Link
+            key={blog._id}
+            to="/edit/"
+            className="flex items-center justify-between rounded bg-slate-800 p-4"
+            state={{ blog }}
+          >
+            <h2 className="text-xl ">Blog Title: {blog.title}</h2>
+            <p>Status: {blog.status}</p>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
